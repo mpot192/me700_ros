0ddc0617b9e9758858b32921996b9c884fd03d33
mpot192 <mpot192@aucklanduni.ac.nz>
2025-05-16 17:59:42 +1200

    idk how quarternions work

--- src/path_follow/src/publish_thing.cpp
+++ src/path_follow/src/publish_thing.cpp
@@ -7,30 +7,34 @@ int main(int argc, char **argv){
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
 
-  ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("chatter", 1000);
-  ros::Rate loop_rate(10);
-
-  int count = 0;
+  ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 1000);
+  ros::Rate loop_rate(5);
+  long count = 0;
+  float step = 0.0;
   while (ros::ok()){
     geometry_msgs::PoseStamped msg;
     
     msg.header.stamp = ros::Time::now();
     msg.header.frame_id = "map";
 
-    msg.pose.position.x = 10.0;
-    msg.pose.position.y = 10.0;
-    msg.pose.position.z = 10.0;
+   if (count % 10 == 0 && count > 50){
+      step += 0.01;
+    }
+
+    msg.pose.position.x = 0.0;
+    msg.pose.position.y = 0.0;
+    msg.pose.position.z = 3.0;
 
     msg.pose.orientation.x = 0.0;
     msg.pose.orientation.y = 0.0;
-    msg.pose.orientation.z = 0.0;
+    msg.pose.orientation.z = step; 
     msg.pose.orientation.w = 0.0;
 
     chatter_pub.publish(msg);
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
+    count++;
   }
   return 0;
 }
