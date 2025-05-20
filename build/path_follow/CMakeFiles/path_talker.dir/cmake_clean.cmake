ef11326ce61127b45e7a58bf343649c4629ed0a9
mpot192 <mpot192@aucklanduni.ac.nz>
2025-05-20 22:21:31 +1200

    Publish static path and follow OL

---
+++ build/path_follow/CMakeFiles/path_talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/matt/catkin_ws/devel/.private/path_follow/lib/path_follow/path_talker"
+  "/home/matt/catkin_ws/devel/.private/path_follow/lib/path_follow/path_talker.pdb"
+  "CMakeFiles/path_talker.dir/src/publish_path.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/path_talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
