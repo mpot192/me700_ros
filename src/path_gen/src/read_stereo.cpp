1834390ed6b4d67b844bc11e3e41a53d1012e307
mpot192 <mpot192@aucklanduni.ac.nz>
2025-06-16 22:30:48 +1200

    generates path but its garbage

--- src/path_gen/src/read_stereo.cpp
+++ src/path_gen/src/read_stereo.cpp
@@ -4,6 +4,7 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <nav_msgs/Odometry.h>
 #include <opencv2/opencv.hpp>
 #include <fstream>
 #include <iostream>
@@ -32,7 +33,10 @@ using namespace std;
 
 bool created_image = false;
 PCHandler handler;
-
+nav_msgs::Odometry drone_pos;
+void get_position(const nav_msgs::Odometry& msg){
+  drone_pos = msg; 
+}
 void Cylinder(float r, int n, float h, float cyl[][3]){
   // float r = diameter/2;
   float dt = (2*M_PI)/(n-1);
@@ -56,12 +60,16 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
   float starth;
   float endh;
 
-  auto& dcpt = handler.GetPC()->at(bby,bbx);
+  int width = handler.GetPC()->width;
+  int height = handler.GetPC()->height;
 
-  float dcx = dcpt.x;
-  float dcy = dcpt.y;
+  auto& dcpt = handler.GetPC()->at(bbx,bby);
+
+  float dcx = dcpt.x + drone_pos.pose.pose.position.x;
+  float dcy = dcpt.y + drone_pos.pose.pose.position.y;
 
   // cout << "1" << endl;
+  ROS_INFO("1");
   if(bbhu < bbwu){
     bbru = bbhu / 2;
   } else {
@@ -69,11 +77,12 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
   }
   // cout << "2 " << endl;
 
+  ROS_INFO("2");
   // find max and min height within area of interest
-  for(int i = 0; i < IMGW; i++){
-    for(int j = 0; j < IMGH; j++){
+  for(int i = 0; i < width; i++){
+    for(int j = 0; j < height; j++){
       if(pow((i - bbx),2) + pow((j - bby),2) <= pow(bbru, 2)){
-        auto& pt = handler.GetPC()->at(j,i) ;
+        auto& pt = handler.GetPC()->at(i,j);
         if(sz == 0){
           maxh = pt.z;
           minh = pt.z;
@@ -92,6 +101,7 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
   // cout << "Min Depth: " << minh << endl;
   // cout << "Max Depth: " << maxh << endl;
 
+  ROS_INFO("3");
   starth = minh;
   endh = minh + (maxh - minh)*d_cut;
 
@@ -106,6 +116,7 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
   }
 
 
+  ROS_INFO("4");
   // cout << "5" << endl;
   // estimate diameter at each layer
   float h_tol = 0.01;
@@ -114,9 +125,14 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
   float maxr = 0;
   float layer_r[nlayers];
   for (int i = 0; i < nlayers; i++){
-    for(int j = 0; j < IMGW; j++){
-      for(int k = 0; k < IMGH; k++){
-        auto& pt2 = handler.GetPC()->at(k,j);
+    for(int j = 0; j < width; j++){
+      for(int k = 0; k < height; k++){
+        // ROS_INFO("4.1");
+        if(k == 0 && j == 480){
+          ROS_INFO("i: %d k: %d j: %d height: %d width: %d", i, k, j, height, width);
+        }
+        auto& pt2 = handler.GetPC()->at(j,k);
+        // ROS_INFO("4.2");
         if(pow((j - bbx),2) + pow((k - bby),2) <= pow(bbru, 2) && abs(pt2.z - layer_depths[i]) <= h_tol){
           xval = abs(pt2.x - dcx);
           yval = abs(pt2.y - dcy);
@@ -133,6 +149,7 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
   }
   // cout << "6" << endl;
 
+  ROS_INFO("5");
   int n_points = 20;
   float cyl[n_points+1][3];
   float pathx[PATH_SIZE];
@@ -158,6 +175,7 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
     
   }
 
+  ROS_INFO("7");
   // cout << "_______________" << endl;
   // cout << "7 " << path[1][0] << endl;
   for(int  i = 0; i<p_size;i++){
@@ -165,10 +183,12 @@ int GeneratePath(float (&path)[PATH_SIZE][3], int bbx, int bby, int bbh, int bbw
     path[1][i] = pathy[i];
     path[2][i] = pathz[i];
     // cout << "[" << path[0][i] << ", " << path[1][i]<< ", " << path[2][i] << "]" << endl;
+    ROS_INFO("[%d] [%f, %f, %f]", i, pathx[i], pathy[i], pathz[i]); 
   }
 
   // cout << "7 " << path[1][0] << endl;
   
+  ROS_INFO("8");
   return p_size;
 }
 
@@ -180,6 +200,7 @@ int main (int argc, char *argv[]) {
 
   // comms
   ros::Subscriber sub = nh.subscribe("/camera/depth/points", 10, &PCHandler::CallbackPC, &handler);
+  ros::Subscriber pos = nh.subscribe("/mavros/global_position/local", 10, get_position);
   ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("/planned_path", 1000);
   nav_msgs::Path planned;
 
@@ -219,6 +240,7 @@ int main (int argc, char *argv[]) {
 
     if(first && handler.exists){
       size = GeneratePath(path, BBX, BBY, BBH, BBW, R_AVOID, H_LAYER, D_CUT, UNCERTAINTY);
+      ROS_INFO("Generated path of size: %d!",size);
       first = false;
     }
 
