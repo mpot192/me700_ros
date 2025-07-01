16164bff18c2f861ba40cd693fc13a9132850927
IvanHsu04 <ihsu296@aucklanduni.ac.nz>
2025-07-01 19:28:40 +1200

    minor changes to publish positions to Rviz, still gotta test

--- src/path_follow/src/publish_velocity_carrot.cpp
+++ src/path_follow/src/publish_velocity_carrot.cpp
@@ -3,6 +3,7 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/TwistStamped.h"
 #include "nav_msgs/Odometry.h"
+#include <nav_msgs/Path.h>
 #include <sstream>
 #include <cmath>
 #include <eigen3/Eigen/Dense>
@@ -29,12 +30,24 @@ int main(int argc, char **argv) {
     ros::NodeHandle n;
 
     geometry_msgs::TwistStamped vel_msg;
+    geometry_msgs::PoseStamped pose;
 
     // Publisher to send target position
     ros::Publisher velocity_pub = n.advertise<geometry_msgs::TwistStamped>("/mavros/setpoint_velocity/cmd_vel", 1000);
 
+    // Subscriber to current odom for drone position
     ros::Subscriber pos_sub = n.subscribe("/mavros/global_position/local", 1000, odomCallback);
     ros::Rate loop_rate(5);
+
+    // Publisher to send planned path (to RViz)
+    ros::Publisher path_pub = n.advertise<nav_msgs::Path>("/planned_path", 1, true);
+    nav_msgs::Path planned_path;
+    planned_path.header.frame_id = "map";
+    
+    // Publisher to send actual drone positions to RViz
+    ros::Publisher actual_path_pub = n.advertise<nav_msgs::Path>("/actual_path", 10);
+    nav_msgs::Path actual_path;
+    actual_path.header.frame_id = "map";
 
     // Parameters
     float lookahead_distance = 0.6f; // meters
@@ -46,6 +59,27 @@ int main(int argc, char **argv) {
     const int cols = 182;
     double path[rows][cols] = {{3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3, 3, 3.12041, 3.22457, 3.29839, 3.33191, 3.32061, 3.26601, 3.17548, 3.06125, 2.93875, 2.82452, 2.73399, 2.67939, 2.66809, 2.70161, 2.77543, 2.87959, 3, 3, 3.06021, 3.11228, 3.14919, 3.16596, 3.1603, 3.133, 3.08774, 3.03062, 2.96938, 2.91226, 2.867, 2.8397, 2.83404, 2.85081, 2.88772, 2.93979, 3, 3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3, 3, 3.18062, 3.33685, 3.44758, 3.49787, 3.48091, 3.39901, 3.26322, 3.09187, 2.90813, 2.73678, 2.60099, 2.51909, 2.50213, 2.55242, 2.66315, 2.81938, 3},{3, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5, 3.33333, 3.31082, 3.24634, 3.14858, 3.03076, 2.90878, 2.79912, 2.71659, 2.67234, 2.67234, 2.71659, 2.79912, 2.90878, 3.03076, 3.14858, 3.24634, 3.31082, 3.33333, 3.16667, 3.15541, 3.12317, 3.07429, 3.01538, 2.95439, 2.89956, 2.8583, 2.83617, 2.83617, 2.8583, 2.89956, 2.95439, 3.01538, 3.07429, 3.12317, 3.15541, 3.16667, 3.5, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5, 3.5, 3.46624, 3.3695, 3.22287, 3.04613, 2.86317, 2.69868, 2.57489, 2.50851, 2.50851, 2.57489, 2.69868, 2.86317, 3.04613, 3.22287, 3.3695, 3.46624, 3.5},{3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25}};
 
+
+    // Publish the planned path to RViz (only need to do once)
+    // nav_msgs::Path planned_path;
+    // planned_path.header.frame_id = "map";
+    for (int i = 0; i < cols; ++i) {
+        // geometry_msgs::PoseStamped pose;
+        pose.header.frame_id = "map";
+        pose.pose.position.x = path[0][i];
+        pose.pose.position.y = path[1][i];
+        pose.pose.position.z = path[2][i];
+        pose.pose.orientation.w = 1.0;
+        planned_path.poses.push_back(pose);
+    }
+    // Publishing planned_path positions to topic
+    path_pub.publish(planned_path);
+
+    // To accumulate drone's actual path
+    // nav_msgs::Path actual_path;
+    // actual_path.header.frame_id = "map";
+
+
     while (ros::ok()) {
         if (!run) {
             ros::spinOnce();
@@ -53,17 +87,33 @@ int main(int argc, char **argv) {
             continue;
         }
 
-        ROS_INFO_THROTTLE(0.75,"Drone_pos: [%f, %f, %f]", 
-            drone_odom.pose.pose.position.x, 
-            drone_odom.pose.pose.position.y, 
-            drone_odom.pose.pose.position.z
-        );
+        // ROS_INFO_THROTTLE(0.75,"Drone_pos: [%f, %f, %f]", 
+        //     drone_odom.pose.pose.position.x, 
+        //     drone_odom.pose.pose.position.y, 
+        //     drone_odom.pose.pose.position.z
+        // );
 
         Eigen::Vector3f drone_pos(
             drone_odom.pose.pose.position.x,
             drone_odom.pose.pose.position.y,
             drone_odom.pose.pose.position.z
         );
+
+        // Publishing current drone positions to topic
+        geometry_msgs::PoseStamped current_pose;
+        current_pose.header.stamp = ros::Time::now();
+        current_pose.header.frame_id = "map";
+        current_pose.pose.position.x = drone_pos.x();
+        current_pose.pose.position.y = drone_pos.y();
+        current_pose.pose.position.z = drone_pos.z();
+        // current_pose.pose.orientation.x = 0.0;
+        // current_pose.pose.orientation.y = 0.0;
+        // current_pose.pose.orientation.z = 0.0;
+        current_pose.pose.orientation.w = 1.0;
+        actual_path.poses.push_back(current_pose);
+        // if (actual_path.poses.size() > 1000) actual_path.poses.erase(actual_path.poses.begin());
+        actual_path_pub.publish(actual_path);
+
 
         Eigen::Vector3f carrot;
         Eigen::Vector3f velocity;
